47bb2088f6b11c0f19943fbb6b9eec65621a30d6
mieomieo <mieo.mieo.1102@gmail.com>
2024-04-26 01:04:11 +0700

    aaaaa

--- app/src/main/java/com/example/navigation/model/User.kt
+++ app/src/main/java/com/example/navigation/model/User.kt
@@ -2,11 +2,12 @@ package com.example.navigation.model
 
 import android.os.Parcelable
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-@Entity(tableName ="user_table")
+@Entity(tableName ="user_table", indices = [Index(value = ["email"], unique = true)])
 class User(@PrimaryKey(autoGenerate = true)
     val id:Int = 0,
     val email: String,
